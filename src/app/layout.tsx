c900b30a1246c51fd0fd741c2b5a30261acf26e3
Vladimir548 <kormisenkovvladimir@gmail.com>
2024-02-15 22:05:57 +0400

    add toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from 'next';
-import { Noto_Serif, Montserrat } from 'next/font/google';
+import { Montserrat } from 'next/font/google';
 import './globals.scss';
 import QueryProviders from '@/providers/QueryProviders';
 import Sidebar from '@/app/components/sidebar/Sidebar';
-import Header from '@/app/components/header/Header';
+
 import ReduxProvider from '@/providers/ProviderRedux';
 
 import NavigationBottom from '@/app/components/navigation-bottom/NavigationBottom';
+import { Toaster } from 'react-hot-toast';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
 
@@ -24,13 +25,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               <div className={'hidden w-[260px] relative md:block '}>
                 <Sidebar />
               </div>
-
+              <Toaster position={'top-center'} />
               <main className={'relative w-full '}>
                 {/*<div className="sticky left-0 top-0 z-[99] bg-primary/80 backdrop-blur">*/}
                 {/*  <Header />*/}
                 {/*</div>*/}
 
                 <div className=" px-0 md:px-2">{children}</div>
+
                 <div className="mt-[50px] md:mt-0">
                   <NavigationBottom />
                 </div>
