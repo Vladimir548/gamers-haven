f53f99c0634c3dfd71888e71d1c5ab371c1ab55b
Vladimir548 <kormisenkovvladimir@gmail.com>
2024-02-20 19:57:39 +0400

    delete hydration

--- src/app/games/page.tsx
+++ src/app/games/page.tsx
@@ -1,20 +1,9 @@
-import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
-import { QueryGames } from '@/app/query/query-games';
 import Games from '@/app/client-pages/games/Games';
 
 export default async function PageGames() {
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ['games'],
-    // @ts-ignore
-    queryFn: QueryGames.getGames,
-  });
   return (
     <div>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Games />
-      </HydrationBoundary>
+      <Games />
     </div>
   );
 }
